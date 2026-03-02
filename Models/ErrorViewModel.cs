e46bb4e24f0513258219ea9966e51112f3557d0c
Alexandre Matos <matosalexandre795@gmail.com>
2026-03-02 19:55:32 -0300

    Projeto Criado!

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaBancario.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
