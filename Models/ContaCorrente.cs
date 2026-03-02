6571a7a561e11f504c1ff36b0f90345687629371
Alexandre Matos <matosalexandre795@gmail.com>
2026-03-02 20:41:06 -0300

    Aplicando Classe ContaCorrente!

--- Models/ContaCorrente.cs
+++ Models/ContaCorrente.cs
@@ -1,6 +1,14 @@
 ﻿namespace SistemaBancario.Models
 {
-    public class ContaCorrente
+    public class ContaCorrente :Conta
     {
+        public ContaCorrente(string nome, decimal saldoInicial) : base(nome, "Corrente", saldoInicial)
+        {
+        }
+
+        public override bool ValidarAbertura()
+        {
+            return true;
+        }
     }
 }
