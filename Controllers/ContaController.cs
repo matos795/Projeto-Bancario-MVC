c4148f322594c327472be79e4abd19d865cefaf1
Alexandre Matos <matosalexandre795@gmail.com>
2026-03-02 21:42:51 -0300

    ContaController

--- Controllers/ContaController.cs
+++ Controllers/ContaController.cs
@@ -1,12 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaBancario.Models;
 
 namespace SistemaBancario.Controllers
 {
     public class ContaController : Controller
     {
-        public IActionResult Index()
+        private static List<Conta> listaContas = new List<Conta>();
+
+        public IActionResult Cadastrar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(string nomeCliente, decimal saldoInicial, string tipoConta)
+        {
+            Conta conta;
+
+            if (tipoConta == "Corrente")
+            {
+                conta = new ContaCorrente(nomeCliente, saldoInicial);
+            }
+            else if (tipoConta == "Poupança")
+            {
+                conta = new ContaPoupanca(nomeCliente, saldoInicial);
+            } 
+            else
+            {
+                TempData["Erro"] = "Tipo de conta inválido.";
+                return RedirectToAction("Cadastrar");
+            }
+            if (conta.ValidarAbertura())
+            {
+                listaContas.Add(conta);
+                TempData["Sucesso"] = "Conta cadastrada com sucesso!";
+                return RedirectToAction("Index", "Home");
+            } 
+            else
+            {
+                TempData["Erro"] = "A Conta não pôde ser aberta. Verifique os critérios";
+                return RedirectToAction("Cadastrar");
+            }
         }
     }
 }
